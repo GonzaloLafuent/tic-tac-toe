3ff891c2ed8f07af83fa5d8242c524c41f76976b
Gonzalo Matias  Lafuente <lafuentegonzalo1@gmail.com>
2024-01-24 16:07:20 -0300

    cambios en la funcion para detectra ganador

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ console.log(cuadrado.dataset.value);
 
 const gameBoard = (function(){
     let board = [0,0,0,0,0,0,0,0,0];
-    const winningCombinatios = ["012","345","678",""];
+    const winningCombinatios = ["012","345","678"];
 
     const setMovement = (position) => {board[position]=1};
 
@@ -41,7 +41,11 @@ const gameController = (function(){
     const changeTurn = ()=> {player_turn=player_turn===player_1?player_2:player_1}
 
     const playerWin = ()=>{
-        return player_turn.getPlayCode().every((code)=>{gameBoard.winningCombinatios.includes(code)})
+        return gameBoard.winningCombinatios.some((combination)=>{
+            return combination.split("").every((code)=>{
+                return gameController.getTurn().getPlayCode().includes(code)
+            });
+        });
     }
 
     return{
@@ -49,14 +53,17 @@ const gameController = (function(){
     }
 })();
 
-const doomController =(function(){
+const domController =(function(){
     const boxes = document.querySelectorAll(".cuadrado");
-    const text_winner= document.querySelector("h2");
+    const text_winner= document.querySelector("h3");
     function addBoxController(){
         boxes.forEach(box => {
             box.addEventListener("click",(e)=>{
                 box.textContent = gameController.getTurn().getFigure();
+                gameBoard.setMovement(e.target.dataset.value);
                 gameController.getTurn().setPlayCode(e.target.dataset.value);
+                console.log(e.target.dataset.value);
+                console.log(gameController.getTurn().getPlayCode());
                 if(gameController.playerWin()) text_winner.textContent = "gano" + gameController.getTurn().getFigure(); 
                 gameController.changeTurn();
             })
@@ -72,6 +79,6 @@ const doomController =(function(){
     }
 })();
 
-doomController.activate()
+domController.activate();
 
 
